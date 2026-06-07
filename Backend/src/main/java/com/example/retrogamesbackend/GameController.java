7bc40b96f7da1981689d46c088f2a00aeab92e71
George Papantoniou <155919628+gepapant@users.noreply.github.com>
2026-06-07 19:09:03 +0300

    Create GameController.java

---
+++ Backend/src/main/java/com/example/retrogamesbackend/GameController.java
@@ -0,0 +1,38 @@
+package com.example.retrogamesbackend;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/games")
+public class GameController {
+
+    private final GameRepository repository;
+
+    public GameController(GameRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Game> getGames(
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) String dateFrom,
+            @RequestParam(required = false) String dateTo
+    ) {
+
+        LocalDate from = null;
+        LocalDate to = null;
+
+        if (dateFrom != null && !dateFrom.isEmpty()) {
+            from = LocalDate.parse(dateFrom);
+        }
+
+        if (dateTo != null && !dateTo.isEmpty()) {
+            to = LocalDate.parse(dateTo);
+        }
+
+        return repository.filterGames(title, from, to);
+    }
+}
