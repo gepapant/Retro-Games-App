f5e89190890f97940c22a2ad73904af0bd5349bd
George Papantoniou <155919628+gepapant@users.noreply.github.com>
2026-06-07 18:57:02 +0300

    Create Game.java

---
+++ Backend/src/main/java/com/example/retrogamesbackend/Game.java
@@ -0,0 +1,80 @@
+package com.example.retrogamesbackend;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "retro_games")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    private String platform;
+
+    private String releaseDate;
+
+    private String description;
+
+    private String imageUrl;
+
+    private String youtubeUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getYoutubeUrl() {
+        return youtubeUrl;
+    }
+
+    public void setYoutubeUrl(String youtubeUrl) {
+        this.youtubeUrl = youtubeUrl;
+    }
+}
