e681b15b94728a32095342374570a1a04f2192f4
George Papantoniou <155919628+gepapant@users.noreply.github.com>
2026-06-07 18:56:18 +0300

    Create RetroGamesApplication.java

---
+++ Backend/src/main/java/com/example/retrogamesbackend/RetroGamesApplication.java
@@ -0,0 +1,16 @@
+package com.example.retrogamesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroGamesApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                RetroGamesApplication.class,
+                args
+        );
+    }
+}
