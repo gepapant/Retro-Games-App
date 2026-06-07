c5e760530fd7f88be92efdccb7b0b32ce4006417
George Papantoniou <155919628+gepapant@users.noreply.github.com>
2026-06-07 18:53:19 +0300

    Create sample_data.sql

---
+++ Database/sample_data.sql
@@ -0,0 +1,30 @@
+INSERT INTO retro_games
+(title, platform, release_date, description, image_url, youtube_url)
+VALUES
+
+(
+'Super Mario Bros',
+'NES',
+'1985-09-13',
+'Classic Nintendo platform game',
+'https://example.com/mario.jpg',
+'https://www.youtube.com/watch?v=ia8bhFoqkVE'
+),
+
+(
+'Sonic The Hedgehog',
+'Sega Genesis',
+'1991-06-23',
+'Fast paced platform game',
+'https://example.com/sonic.jpg',
+'https://www.youtube.com/watch?v=Sz4JrU4JYhA'
+),
+
+(
+'Metal Slug',
+'Neo Geo',
+'1996-04-19',
+'Arcade run and gun shooter',
+'https://example.com/metalslug.jpg',
+'https://www.youtube.com/watch?v=Q5O2Xf8V5nE'
+);
