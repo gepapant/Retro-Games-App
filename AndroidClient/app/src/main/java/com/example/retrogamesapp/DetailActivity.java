edaa1c6a24ed8f6d7b64c9db2397ef32413aced7
George Papantoniou <155919628+gepapant@users.noreply.github.com>
2026-06-07 19:23:23 +0300

    Update DetailActivity.java

--- AndroidClient/app/src/main/java/com/example/retrogamesapp/DetailActivity.java
+++ AndroidClient/app/src/main/java/com/example/retrogamesapp/DetailActivity.java
@@ -1,5 +1,60 @@
 package com.example.retrogamesapp;
 
-public class DetailActivity {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class DetailActivity extends AppCompatActivity {
+
+    ImageView imgCover;
+    TextView title, platform, date, description;
+    Button btnTrailer;
+
+    String youtubeUrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        imgCover = findViewById(R.id.imgCover);
+        title = findViewById(R.id.txtTitle);
+        platform = findViewById(R.id.txtPlatform);
+        date = findViewById(R.id.txtReleaseDate);
+        description = findViewById(R.id.txtDescription);
+        btnTrailer = findViewById(R.id.btnTrailer);
+
+        Intent intent = getIntent();
+
+        String gameTitle = intent.getStringExtra("title");
+        String gamePlatform = intent.getStringExtra("platform");
+        String gameDate = intent.getStringExtra("date");
+        String gameDescription = intent.getStringExtra("description");
+        String imageUrl = intent.getStringExtra("imageUrl");
+        youtubeUrl = intent.getStringExtra("youtubeUrl");
+
+        title.setText(gameTitle);
+        platform.setText(gamePlatform);
+        date.setText(gameDate);
+        description.setText(gameDescription);
+
+        Glide.with(this)
+                .load(imageUrl)
+                .into(imgCover);
+
+        btnTrailer.setOnClickListener(v -> {
+
+            Intent browserIntent =
+                    new Intent(Intent.ACTION_VIEW, Uri.parse(youtubeUrl));
+
+            startActivity(browserIntent);
+        });
+    }
 }
