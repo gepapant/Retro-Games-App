42942a1bf5867dcb1d8da69f2655d6763ba488a2
George Papantoniou <155919628+gepapant@users.noreply.github.com>
2026-06-08 15:00:09 +0300

    Update urls.py

--- DjangoBackend/retrogames_api/urls.py
+++ DjangoBackend/retrogames_api/urls.py
@@ -1,1 +1,16 @@
+from django.contrib import admin
 
+from django.urls import path, include
+
+urlpatterns = [
+
+    path(
+        'admin/',
+        admin.site.urls
+    ),
+
+    path(
+        'api/',
+        include('games.urls')
+    ),
+]
