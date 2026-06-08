f5ac67b588485b19290d0dfba9decced4f26412e
George Papantoniou <155919628+gepapant@users.noreply.github.com>
2026-06-08 14:58:07 +0300

    Update models.py

--- DjangoBackend/games/models.py
+++ DjangoBackend/games/models.py
@@ -1,1 +1,19 @@
+from django.db import models
 
+
+class Game(models.Model):
+
+    title = models.CharField(max_length=255)
+
+    platform = models.CharField(max_length=100)
+
+    release_date = models.DateField()
+
+    description = models.TextField(blank=True)
+
+    image_url = models.URLField(blank=True)
+
+    youtube_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
