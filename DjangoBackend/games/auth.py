bc8461741b3e20eea969f4dfcbfdfd2290830645
George Papantoniou <155919628+gepapant@users.noreply.github.com>
2026-06-08 16:23:32 +0300

    Create auth.py

---
+++ DjangoBackend/games/auth.py
@@ -0,0 +1,30 @@
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(['POST'])
+def login(request):
+
+    username = request.data.get('username')
+
+    password = request.data.get('password')
+
+    user = authenticate(
+        username=username,
+        password=password
+    )
+
+    if user:
+
+        token, created = Token.objects.get_or_create(
+            user=user
+        )
+
+        return Response({'token': token.key})
+
+    return Response(
+        {'error': 'Invalid credentials'},
+        status=400
+    )
