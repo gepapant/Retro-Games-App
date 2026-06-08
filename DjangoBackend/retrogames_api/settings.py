f3d70550df7335dee1fac65ee62ee262cb533c88
George Papantoniou <155919628+gepapant@users.noreply.github.com>
2026-06-08 15:00:56 +0300

    Update settings.py

--- DjangoBackend/retrogames_api/settings.py
+++ DjangoBackend/retrogames_api/settings.py
@@ -1,1 +1,35 @@
+INSTALLED_APPS = [
 
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'games',
+]
+
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': 'retrogames',
+
+        'USER': 'root',
+
+        'PASSWORD': 'YOUR_PASSWORD',
+
+        'HOST': 'localhost',
+
+        'PORT': '3306',
+    }
+}
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
