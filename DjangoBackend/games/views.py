1c8ba2ae3fe6e611635951dc5835b465bb41417c
George Papantoniou <155919628+gepapant@users.noreply.github.com>
2026-06-08 14:59:10 +0300

    Update views.py

--- DjangoBackend/games/views.py
+++ DjangoBackend/games/views.py
@@ -1,1 +1,39 @@
+from rest_framework import viewsets
 
+from .models import Game
+
+from .serializers import GameSerializer
+
+
+class GameViewSet(viewsets.ModelViewSet):
+
+    queryset = Game.objects.all()
+
+    serializer_class = GameSerializer
+
+    def get_queryset(self):
+
+        queryset = Game.objects.all()
+
+        title = self.request.GET.get('title')
+
+        date_from = self.request.GET.get('dateFrom')
+
+        date_to = self.request.GET.get('dateTo')
+
+        if title:
+            queryset = queryset.filter(
+                title__icontains=title
+            )
+
+        if date_from:
+            queryset = queryset.filter(
+                release_date__gte=date_from
+            )
+
+        if date_to:
+            queryset = queryset.filter(
+                release_date__lte=date_to
+            )
+
+        return queryset
